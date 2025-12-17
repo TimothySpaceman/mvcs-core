235438b106ee4bc9a6397c75d3af79239bdef0be
Timothy Rehenel <timothy.rehenel@gmail.com>
2025-12-18 01:04:02 +0200

    feat: working directories, localworkingdirectory first prototype

---
+++ Core/Exceptions/BlobContentNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Exceptions;
+
+[Serializable]
+public class BlobContentNotFoundException : Exception
+{
+    public BlobContentNotFoundException()
+    {
+    }
+
+    public BlobContentNotFoundException(string message) : base(message)
+    {
+    }
+
+    public BlobContentNotFoundException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
---
+++ Core/WorkingDirectories/IWorkingDirectory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Immutable;
+using Core.FileSnapshots;
+using Core.Snapshots;
+
+namespace Core.WorkingDirectories;
+
+public interface IWorkingDirectory
+{
+    public Snapshot GetCurrentSnapshot(IgnoreRuleSet? ignoreRules = null);
+    public void ApplySnapshot(Snapshot snapshot, IgnoreRuleSet? ignoreRules = null);
+}
